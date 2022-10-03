86a31051fd2d4a75410b7bba3fab1d851647c7ad
gabriellukke <gabriellukke@outlook.com>
2022-10-03 02:50:37 -0300

    lint: todo serializer

--- api/todos/serializers.py
+++ api/todos/serializers.py
@@ -2,8 +2,8 @@ from rest_framework import serializers
 
 from .models import Todo
 
+
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
         fields = "__all__"
-        
